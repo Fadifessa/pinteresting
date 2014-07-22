39f72c17be1e3e25f03ca0e566f201aec135fed5
Fadi Adel <fadifessa@icloud.com>
2014-07-21 23:20:05 -0400

    updated Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/list'
+
   root "pages#home"
   get "about" => "pages#about"
+  get "list" => "pages#list"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
